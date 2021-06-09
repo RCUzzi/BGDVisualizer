26f6841dfd07133c63e7bbc3ae132f83e6ea34f5
Robert Iannuzzi <robert.claudio96@gmail.com>
2021-06-09 00:22:04 -0400

    Added more command line arguments and plot

--- bgdvisualizer.py
+++ bgdvisualizer.py
@@ -8,10 +8,13 @@ def main(argv):
     parser = argparse.ArgumentParser()
 
     #add optional arguments
-    parser.add_argument('-d', '--datafile', dest='datafile', help = "Path to data file to be used", type=str, required=True)
-    parser.add_argument('-a', '--alpha', dest='alpha', 
-    help = 'Select a learning rate for your dataset (this should be approx .01, .03, .1, .3, 1, or 3). This defaults to .01', default = .01, type=float)
-    parser.add_argument('-i', '--iterations', dest='iterations', help = 'Number of times to run gradient descent. This defaults to 100', default = 100, type=int)
+    parser.add_argument('-d', '--datafile', dest = 'datafile', help = "Path to data file to be used", type = str, required = True)
+    parser.add_argument('-a', '--alpha', dest = 'alpha', 
+    help = 'Select a learning rate for your dataset (this should be approx .01, .03, .1, .3, 1, or 3). This defaults to .01', default = .01, type = float)
+    parser.add_argument('-i', '--iterations', dest = 'iterations', help = 'Number of times to run gradient descent. This defaults to 100', default = 100, type = int)
+    parser.add_argument('-w', '--wait', help = "Keep graph open after animation until manually closed", action = 'store_true')
+    parser.add_argument('-s', '--show', help = 'Show graph animation during calculations (will not display graph if more than 1 feature exists)', action = 'store_true')
+    parser.add_argument('-c', '--cost', help = 'Output history of cost values to a file', action = 'store_true')
 
     # read arguments from command line
     args = parser.parse_args()
@@ -23,14 +26,14 @@ def main(argv):
 
     # check that the file exists
     if(os.path.isfile(datafile)):
-        process_data(datafile, alpha, iterations)
+        process_data(datafile, alpha, iterations, args)
     else:
         print('The file does not exist.')
 
 
-def process_data(datafile, alpha, iterations):
+def process_data(datafile, alpha, iterations, args):
     # load data from file
-    data = np.loadtxt(os.path.join(datafile), delimiter=',')
+    data = np.loadtxt(os.path.join(datafile), delimiter = ',')
 
     # slice csv into a set of parameters and y-values
     raw_features = data[:, :-1]
@@ -40,31 +43,46 @@ def process_data(datafile, alpha, iterations):
     features_norm = normalize_matrix(raw_features)
 
     # add column of ones to X for accomodate for theta0 values
-    features = np.concatenate([np.ones((values.size, 1)), features_norm], axis=1)
-    theta = np.zeros(np.size(features, axis=1))
-
+    features = np.concatenate([np.ones((values.size, 1)), features_norm], axis = 1)
+    theta = np.zeros(np.size(features, axis = 1))
     # run (batch) gradient descent
-    batch_gradient_descent(features, values, theta, alpha, iterations)
+    batch_gradient_descent(features, values, theta, alpha, iterations, show=args.show, show_cost=args.cost, wait=args.wait)
         
 
 def cost_function(features, values, theta):
     m = values.shape[0]
-    J = (1/(2*m))*np.sum((np.dot(features, theta)-values)**2)
-    return J
+    cost = (1/(2*m))*np.sum((np.dot(features, theta)-values)**2)
+    return cost
     
 
-def batch_gradient_descent(features, values, theta, alpha, iterations):
+def batch_gradient_descent(features, values, theta, alpha, iterations, show=False, show_cost=False, wait=False):
     m = values.shape[0]
     cost_history = []
+    if(show == True):
+        figure = plt.figure()
 
     for i in range(iterations):
         theta = theta - (alpha / m)*(np.dot(features, theta)-values).dot(features)
         cost_history.append(cost_function(features, values, theta))
-    
-    print('THETA: ')
-    print(theta)
-    print('Cost History: ')
-    print(cost_history)
+        if((show == True or wait == True) and features[0].size == 2):
+            plt.plot(features[:, 1:], values, 'ro', ms=10, mec='k')
+            plt.ylabel('Values')
+            plt.xlabel('Features')
+            plt.plot(features[:, 1:], np.dot(features, theta), '-')
+            plt.pause(0.00001)
+            if(i != iterations - 1):
+                plt.cla()
+
+    if(wait == True):
+        plt.show()
+    else:
+        plt.show(block=False) 
+         
+    if(show_cost == True):
+        print('Cost History: ')
+        print(cost_history)
+
+    return cost_history
 
 def normalize_matrix(features):
     # initialize features copy and mu/sigma vectors
@@ -73,8 +91,8 @@ def normalize_matrix(features):
     sigma = np.zeros(features.shape[1])
 
     # normalize the features matrix
-    mu = np.mean(features, axis=0)
-    sigma = np.std(features, axis=0)
+    mu = np.mean(features, axis = 0)
+    sigma = np.std(features, axis = 0)
     features = (features - mu) / sigma
     
     return features
@@ -82,9 +100,9 @@ def normalize_matrix(features):
 def show_graphs():
     pass
 
-def show_cost_graph():
+def show_cost_graph(features, values, theta):
     pass
-
+    
 def show_gradient_graph():
     pass
 
